3f37b20eb320b5beb49722fe9f7028c8c5124ac2
Nimarcs <moimonautremoi@gmail.com>
2022-03-10 11:01:31 +0100

    Ajout des modèles

--- CustomBox/src/Models/Boite.php
+++ CustomBox/src/Models/Boite.php
@@ -1,1 +1,14 @@
 <?php
+declare(strict_types=1);
+
+namespace CustomBox\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Boite extends Model
+{
+    protected $table = 'boite';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+}
--- CustomBox/src/Models/Categorie.php
+++ CustomBox/src/Models/Categorie.php
@@ -1,1 +1,31 @@
 <?php
+declare(strict_types=1);
+
+// NAMESPACE
+namespace CustomBox\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+/**
+ * Classe Categorie
+ * Représente une categorie au sein de la base de données
+ * Hérite de la classe Modele du module Eloquent
+ */
+class Categorie extends \Illuminate\Database\Eloquent\Model
+{
+
+    // ATTRIBUTS
+
+    public $timestamps = false;
+    protected $table = 'categorie';
+    protected $primaryKey = 'id';
+
+
+    // CONSTRUCTEUR
+
+    public function produits()
+    {
+        return $this->hasMany('\mywishlist\modele\Produit', 'categorie');
+    }
+
+}
