a46f6ee0dd15cf36ec4b3186483bff628e10eefb
volress <hug0.loiesau@gmail.com>
2022-03-10 13:29:38 +0100

    Transition controlleur produit vers vues

--- CustomBox/src/Controllers/ProduitController.php
+++ CustomBox/src/Controllers/ProduitController.php
@@ -6,11 +6,14 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\RequestInterface as Request;
 
 use CustomBox\Views\ViewRender;
+use Views\ViewGestionProduits;
 
 class ProduitController extends Controller{
 
-    public function creerProduit(Request $request, Response $response, $parameters):Response
+    public function creerProduit(Request $request, Response $response, $parameters):Response //2
     {
+        $vue=new ViewGestionProduits($this->container);
+        $vueRender = new ViewRender($this->container);
         if ($request->isPost()){
 
             $titre = filter_var( $request->getParsedBody()['titre'], FILTER_SANITIZE_STRING);
@@ -24,6 +27,35 @@ class ProduitController extends Controller{
             $parameters["poids"]=$poids;
 
             $this->ajouterProduitBDD($parameters);
+            $response = $response->withRedirect($this->container->router->pathFor('home'));
+        } else {
+            $response = $response->getBody()->write($vueRender->render($vue->render(2)));
+        }
+        return $response;
+    }
+
+    public function modifierProduit(Request $request, Response $response, $parameters) //3
+    {
+        $vue=new ViewGestionProduits($this->container);
+        $vueRender = new ViewRender($this->container);
+
+        if($request->isPost()){
+            $titre = filter_var( $request->getParsedBody()['titre'], FILTER_SANITIZE_STRING);
+            $description = filter_var( $request->getParsedBody()['description'], FILTER_SANITIZE_STRING);
+            $categorie = filter_var( $request->getParsedBody()['request'], FILTER_SANITIZE_STRING);
+            $poids = filter_var( $request->getParsedBody()['poids'], FILTER_SANITIZE_NUMBER_FLOAT);
+
+            $parameters["titre"]=$titre;
+            $parameters["description"]=$description;
+            $parameters["categorie"]=$categorie;
+            $parameters["poids"]=$poids;
+
+            $this->modifierProduitBDD($parameters);
+
+
+            $response = $response->withRedirect($this->container->router->pathFor('home'));
+        } else {
+            $response = $response->getBody()->write($vueRender->render($vue->render(3)));
         }
         return $response;
     }
@@ -52,21 +84,5 @@ class ProduitController extends Controller{
         if (!$res){
             throw new \Exception("Sauvegarde de l'item a échoué");
         }
-    }
-
-    public function modifierProduit(Request $request, Response $response, $parameters)
-    {
-        $titre = filter_var( $request->getParsedBody()['titre'], FILTER_SANITIZE_STRING);
-        $description = filter_var( $request->getParsedBody()['description'], FILTER_SANITIZE_STRING);
-        $categorie = filter_var( $request->getParsedBody()['request'], FILTER_SANITIZE_STRING);
-        $poids = filter_var( $request->getParsedBody()['poids'], FILTER_SANITIZE_NUMBER_FLOAT);
-
-        $parameters["titre"]=$titre;
-        $parameters["description"]=$description;
-        $parameters["categorie"]=$categorie;
-        $parameters["poids"]=$poids;
-
-        $this->modifierProduitBDD($parameters);
-        return $response;
     }
 }
--- CustomBox/src/routes.php
+++ CustomBox/src/routes.php
@@ -4,6 +4,7 @@
  * Home
  */
 $app->get('/', CustomBox\Controllers\HomeController::class . ':index')->setName('home');
+$app->post('/', CustomBox\Controllers\HomeController::class . ':index')->setName('home');
 $app->get('/creationProduit', CustomBox\Controllers\ProduitController::class.':creerProduit')->setName("creationProduit");
 $app->get('/modificationProduit', CustomBox\Controllers\ProduitController::class.':modifierProduit')->setName("modifierProduit");
 $app->post('/modificationProduit', CustomBox\Controllers\ProduitController::class.':modifierProduit')->setName("modifierProduit");
