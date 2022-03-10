a7fa35787ee225e95bfefeb77e76fb3a80b6c398
weiss54 <72012859+weiss54@users.noreply.github.com>
2022-03-10 13:17:41 +0100

    Ajout et modif des models pour gérer les nouvelles tables sql

---
+++ CustomBox/src/Models/Commande.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+// NAMESPACE
+namespace CustomBox\Models;
+
+/**
+ * Classe Commande
+ * Représente une commande au sein de la base de données
+ * Hérite de la classe Modele du module Eloquent
+ */
+class Commande extends \Illuminate\Database\Eloquent\Model
+{
+
+    // ATTRIBUTS
+
+    public $timestamps = false;
+    protected $table = 'commande';
+    protected $primaryKey = 'id';
+
+
+    // CONSTRUCTEUR
+
+    public function user() {
+        return $this->belongsTo('\CustomBox\Models\User', 'idUser');
+    }
+
+    public function boite() {
+        return $this->belongsTo('\CustomBox\Models\Boite', 'idBoite');
+    }
+
+    public function produits() {
+        return $this->belongsToMany('\CustomBox\Models\Produit', 'produitCommande', 'idCommande', 'idProduit');
+    }
+
+}
