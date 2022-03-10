7e5706e629de68a5335e6716ff5c2f13afe40223
Fabrega Jocelyn <91054077+JocelynFabrega@users.noreply.github.com>
2022-03-10 14:52:37 +0100

    le panier et mainteant une box

--- CustomBox/src/Views/ViewRender.php
+++ CustomBox/src/Views/ViewRender.php
@@ -75,7 +75,7 @@ class ViewRender
                                     <li class="nav-item"><a class="nav-link" href="{$this->container->router->pathFor("signin")}">Inscription</a></li>
                                 </ul>
                                 <button class="btn btn-outline-secondary" type="submit">
-                                    <i class="bi-cart-fill me-1"></i>
+                                    <i class="bi bi-box"></i>
                                     Cart
                                     <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
                                 </button>
