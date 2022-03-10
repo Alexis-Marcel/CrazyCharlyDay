3499ce799875280f6502320f0b68ace6c60a4896
weiss54 <72012859+weiss54@users.noreply.github.com>
2022-03-10 17:50:46 +0100

    commentaires

--- CustomBox/src/Controllers/GestionCommandeController.php
+++ CustomBox/src/Controllers/GestionCommandeController.php
@@ -4,15 +4,20 @@ namespace CustomBox\Controllers;
 use CustomBox\Models\Boite;
 use CustomBox\Models\Commande;
 use CustomBox\Models\Produit;
-use CustomBox\Views\ViewGestionProduits;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\RequestInterface as Request;
 
 use CustomBox\Views\ViewRender;
 use CustomBox\Views\ViewGestionCommande;
 
+/**
+ * Classe Controller qui gère les commandes
+ */
 class GestionCommandeController extends Controller{
 
+    /**
+     * Fonction qui affiche le panier
+     */
     public function afficherPanier(Request $request, Response $response, array $parameters):Response
     {
         $vue = new ViewGestionCommande($this->container);
@@ -20,6 +25,9 @@ class GestionCommandeController extends Controller{
         return $response;
     }
 
+    /**
+     * Fonction pour ajouter un produit dans le panier
+     */
     public function ajouterPanier(Request $request, Response $response, array $param):Response {
         $tab = $_SESSION["panier"];
         $tab[] = $param;
@@ -27,6 +35,12 @@ class GestionCommandeController extends Controller{
         return $response->withRedirect($this->container->router->pathFor('home'));
     }
 
+    /**
+     * Fonction qui valide le panier
+     * Si l'user n'est pas connecté, s'affiche une erreur
+     * Sinon si l'user a un boite trop remplis, message d'erreur
+     * Sinon s'affiche un message de validation et enregistre la commande dans la table sql
+     */
     public function validerCommande(Request $request, Response $response, array $parameters):Response {
         $vue = new ViewRender($this->container);
         if(isset($_SESSION['user'])) {
