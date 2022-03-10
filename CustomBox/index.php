5dfec2438aae9333cd2804bc6e07cb3f397d4399
weiss54 <72012859+weiss54@users.noreply.github.com>
2022-03-10 11:31:14 +0100

    Deplacement index.php

---
+++ CustomBox/index.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+require __DIR__ . '\vendor\autoload.php';
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+\CustomBox\config\Eloquent::start(__DIR__ . '/src/config/dbconfig.ini');
+
+$app = new \Slim\App([
+'settings' => [
+'displayErrorDetails' => true,
+]
+]);
+
+$container = $app->getContainer();
+
+
+require __DIR__ . '/src/routes.php';
