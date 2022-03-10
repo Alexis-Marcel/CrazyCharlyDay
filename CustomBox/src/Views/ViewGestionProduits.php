92cdf4be55cd7887ef343c5787b362c77d80e362
Fabrega Jocelyn <91054077+JocelynFabrega@users.noreply.github.com>
2022-03-10 16:01:13 +0100

    modification du text qui était relou

--- CustomBox/src/Views/ViewGestionProduits.php
+++ CustomBox/src/Views/ViewGestionProduits.php
@@ -47,8 +47,8 @@ class ViewGestionProduits
         <header class="bg-dark py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder">Shop in style</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                    <h1 class="display-4 fw-bolder">L'ATELIER 17.91</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">ATELIERS ITINÉRANTS</p>
                 </div>
             </div>
         </header>
