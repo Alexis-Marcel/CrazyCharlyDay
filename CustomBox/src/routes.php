75733ba1549fc2ba0d9e168e90d121f323dbec24
Nimarcs <moimonautremoi@gmail.com>
2022-03-10 14:25:35 +0100

    ajout d'une route de test

--- CustomBox/src/routes.php
+++ CustomBox/src/routes.php
@@ -4,6 +4,11 @@
  * Home
  */
 
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+$app->post('/', CustomBox\Controllers\HomeController::class . ':index')->setName('home');
 $app->get('/', \CustomBox\Controllers\ProduitController::class . ':affichageCatalogue')->setName('home');
 
 $app->get('/creationProduit', CustomBox\Controllers\ProduitController::class.':creerProduit')->setName("creationProduit");
@@ -11,8 +16,16 @@ $app->get('/creationProduit', CustomBox\Controllers\ProduitController::class.':c
 $app->get('/modificationProduit', CustomBox\Controllers\ProduitController::class.':modifierProduit')->setName("modifierProduit");
 $app->post('/modificationProduit', CustomBox\Controllers\ProduitController::class.':modifierProduit')->setName("modifierProduit");
 
+
 $app->get('/creationBoite', \CustomBox\Controllers\BoiteController::class.'creerBoite')->setName("creationBoite");
 $app->get('/modificationBoite', CustomBox\Controllers\ProduitController::class.':modifierBoite')->setName("modifierBoite");
 $app->post('/modificationBoite', CustomBox\Controllers\ProduitController::class.':modifierBoite')->setName("modifierBoite ");
 
 $app->get('/signin', CustomBox\Controllers\Authentification::class . ':getSignIn')->setName('signin');
+
+
+$app->get('/test', function (Request $rq, Response $rs, array $args): Response {
+    $rs->getBody()->write("test");
+    return $rs;
+})->setName('afficherListe');
+
